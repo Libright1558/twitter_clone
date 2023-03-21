254a108461998b7f1faa5aef1d1736e875e600cf
libright1558 <n32717325@gmail.com>
2023-03-21 08:09:21 +0800

    update database.js

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    host:'localhost',
-    database:'community_users',
-    port:process.env.db_port,
+    host: process.env.db_host,
+    database: process.env.db_schema,
+    port: process.env.db_port,
 });
 
 module.exports = pool;
