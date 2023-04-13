c97915692782a7827c1848169fadc33cc79ece6d
libright1558 <n32717325@gmail.com>
2023-04-13 12:31:01 +0800

    add post_id attribute to resultPost

--- public/js/home.js
+++ public/js/home.js
@@ -18,11 +18,13 @@ document.addEventListener("DOMContentLoaded", () => {
             "lastName": results[len - 1].lastName,
             "timestamp": null,
             "postData": null,
+            "post_id": null,
         }
 
         results.slice(0, len - 1).forEach(result => {
             resultPost.timestamp = result.ts;
             resultPost.postData = result.content;
+            resultPost.post_id = result.post_id;
 
             let html = createPostHtml(resultPost);
             postsContainer.insertAdjacentHTML("beforeend", html);
