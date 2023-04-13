cc8a532da4dfc7a2a46e07e0cf98b52abed40402
libright1558 <n32717325@gmail.com>
2023-04-13 21:04:08 +0800

    feat: add user information to .env

--- database.js
+++ database.js
@@ -5,6 +5,7 @@ const pool = new Pool({
     host: process.env.db_host,
     database: process.env.db_schema,
     port: process.env.db_port,
+    user: process.env.db_user,
 });
 
 module.exports = pool;
