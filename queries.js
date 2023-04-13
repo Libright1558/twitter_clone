14b648c890bcc4823bf8cf889526372698a7e792
libright1558 <n32717325@gmail.com>
2023-04-13 21:07:55 +0800

    feat: add fetchPost

--- queries.js
+++ queries.js
@@ -2,10 +2,12 @@ const regist = 'INSERT INTO user_records(firstname, lastname, username, email, p
 const findDup = 'SELECT username, email FROM user_records WHERE username = $1 OR email = $2';
 const findOne = 'SELECT * FROM user_records WHERE username = $1 OR email = $1';
 const postData = 'INSERT INTO post_records(postby, content, pinned, ts) VALUES($1, $2, $3, $4)';
+const fetchPost = 'SELECT * FROM post_records WHERE postby = $1 ORDER BY ts DESC';
 
 module.exports = {
     regist,
     findDup,
     findOne,
     postData,
+    fetchPost,
 };
