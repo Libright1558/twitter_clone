00fed9b9d2a62f4c3392ab4902143f9b589cb0c1
libright1558 <n32717325@gmail.com>
2023-04-25 10:49:46 +0800

    feat: add router.put to handle the click like event on the posts, and minor changes.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -28,57 +28,105 @@ router.get("/", async (req, res, next) => {
         res.status(200).send(JSON.stringify(userPosts));
     }
     catch(err) {
-        console.log("getPost error", err);
+        console.log("posts.js router.get error", err);
+        return;
     }
 })
 
 router.post("/", express.json(), (req, res, next) => {
-
-    if(!(req.body.content)) {
-        console.log("Content param can not send with request");
-        return res.sendStatus(400);
-    }
-    req.body.content = req.body.content.replace(/[\u00A0-\u9999<>\&]/gim, (i) => `&#${i.codePointAt(0)};`);// HTML escape
-    const time = moment().local().format('YYYY-MM-DD HH:mm:ss');
-    const postData = [req.session.user.username, req.body.content, false, time];
-    controller.postData(postData)
-    .then(async () => {
-        const username = req.session.user.username;
-        const email = req.session.user.email;
-        const profilePic = req.session.user.profilePic;
-        const firstName = req.session.user.firstName;
-        const lastName = req.session.user.lastName;
-        const content = req.body.content;
-
-        const postId = await controller.newPostId(username);
-
-        const newPost = {
-            "content": content,
-            "pinned": false,
-            "postby": username,
-            "ts": time,
-            "post_id": postId.rows[0].post_id,
+    try {
+        if(!(req.body.content)) {
+            console.log("Content param can not send with request");
+            return res.sendStatus(400);
         }
+        req.body.content = req.body.content.replace(/[\u00A0-\u9999<>\&]/gim, (i) => `&#${i.codePointAt(0)};`);// HTML escape
+        const time = moment().local().format('YYYY-MM-DD HH:mm:ss');
+        const postData = [req.session.user.username, req.body.content, false, time];
+        controller.postData(postData)
+        .then(async () => {
+            const username = req.session.user.username;
+            const email = req.session.user.email;
+            const profilePic = req.session.user.profilePic;
+            const firstName = req.session.user.firstName;
+            const lastName = req.session.user.lastName;
+            const content = req.body.content;
+    
+            const postId = await controller.newPostId(username);
+    
+            const newPost = {
+                "content": content,
+                "pinned": false,
+                "postby": username,
+                "ts": time,
+                "post_id": postId.rows[0].post_id,
+            }
+    
+            await redis_cache.addPost(username, newPost);
+    
+            const deliver = {
+                "postData": content, 
+                "username": username, 
+                "email": email, 
+                "profilePic": profilePic, 
+                "firstName": firstName, 
+                "lastName": lastName, 
+                "timestamp": time,
+                "post_id": postId,
+            };
+    
+            res.status(201).send(JSON.stringify(deliver));
+        })
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(400);
+        });
+    }
+    catch(err) {
+        console.log("posts.js router.post error", err);
+        return;
+    }
+})
 
-        await redis_cache.addPost(username, newPost);
+router.put("/:id/like", express.json(), async (req, res, next) => {
+    try {
+        const postId = req.body.postId;
+        const username = req.session.user.username;
+        const owner = req.body.postOwner;
 
-        const deliver = {
-            "postData": content, 
-            "username": username, 
-            "email": email, 
-            "profilePic": profilePic, 
-            "firstName": firstName, 
-            "lastName": lastName, 
-            "timestamp": time,
-            "post_id": postId,
-        };
+        const isUserLikeKeyExist = await redis_cache.isKeyExist(username + "_like");
+        if(isUserLikeKeyExist === false) {
+            await redis_cache.loadUserLike(username);
+        }
+        
+        const isAlreadyLike = await redis_cache.isAlreadyLike(username, postId);
+        if(isAlreadyLike === false) {
+            controller.insertUserLike(username, postId)
+            .then(async () => {
+                await redis_cache.addUserLike(username, owner, postId);
+            })
+            .catch(error => {
+                console.log(error);
+                res.sendStatus(400);
+            });
+        }
+        else if(isAlreadyLike === true) {
+            controller.delUserLike(username, postId)
+            .then(async () => {
+                await redis_cache.delUserLike(username, postId);
+            })
+            .catch(error => {
+                console.log(error);
+                res.sendStatus(400);
+            });
+        }
+        const total_likes = await redis_cache.postLike(owner, postId, isAlreadyLike);
 
-        res.status(201).send(JSON.stringify(deliver));
-    })
-    .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
-    });
+        res.status(200).send(JSON.stringify(total_likes));
+    }
+    catch(err) {
+        console.log("posts.js router.put error", err);
+        return;
+    }
 })
 
 module.exports = router;
