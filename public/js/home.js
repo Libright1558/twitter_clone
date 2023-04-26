0b4a1c4494f324249e01e65ae8a8ecc7ad75e7d2
libright1558 <n32717325@gmail.com>
2023-04-26 10:11:57 +0800

    feat: remove some redundant information.

--- public/js/home.js
+++ public/js/home.js
@@ -8,14 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((results) => {
         const postsContainer = document.querySelector('.postsContainer');
 
-        let len = results.length;
-
         let resultPost = {
-            "username": results[len - 1].username, 
-            "email": results[len - 1].email, 
-            "profilePic": results[len - 1].profilePic, 
-            "firstName": results[len - 1].firstName, 
-            "lastName": results[len - 1].lastName,
             "timestamp": null,
             "postData": null,
             "post_id": null,
@@ -23,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
             "retweet_nums": null,
         }
 
-        results.slice(0, len - 1).forEach(result => {
+        results.forEach(result => {
             resultPost.timestamp = result.ts;
             resultPost.postData = result.content;
             resultPost.post_id = result.post_id;
