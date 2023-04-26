ee50e3eef2da7241d1372ce4d205cc425afd9ee9
libright1558 <n32717325@gmail.com>
2023-04-26 10:00:56 +0800

    feat: add UserloggedInJs: JSON.stringify(req.session.user).

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.get("/", middleware.requireLogin, (req, res, next) => {
     var payload = {
         pageTitle: "Home",
         UserloggedIn: req.session.user,
+        UserloggedInJs: JSON.stringify(req.session.user),
     }
     res.status(200).render("home", payload);
 })
