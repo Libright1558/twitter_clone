446b717b78ebc08ed580c039df854c15aa2dd249
libright1558 <n32717325@gmail.com>
2023-04-27 21:29:21 +0800

    feat: add some statement to store the data of UserRetweet.

--- queries.js
+++ queries.js
@@ -9,22 +9,41 @@ const fetchPost = 'SELECT * FROM post_records WHERE postby = $1 ORDER BY ts DESC
 const newPostId = 'SELECT post_id FROM post_records WHERE postby = $1 ORDER BY ts DESC LIMIT 1';
 const insertLikePeople = 'UPDATE post_records SET like_people = ARRAY_APPEND(like_people, $1) WHERE post_id = $2';
 const removeLikePeople = 'UPDATE post_records SET like_people = ARRAY_REMOVE(like_people, $1) WHERE post_id = $2';
+const insertRetweetPeople = 'UPDATE post_records SET retweet_people = ARRAY_APPEND(retweet_people, $1) WHERE post_id = $2';
+const removeRetweetPeople = 'UPDATE post_records SET retweet_people = ARRAY_REMOVE(retweet_people, $1) WHERE post_id = $2';
 
 //user_like
 const userLike = 'INSERT INTO user_likes(username, post_id) VALUES($1, $2)';
 const delUserLike = 'DELETE FROM user_likes WHERE username = $1 AND post_id = $2';
 const fetchUserLike = 'SELECT p.postby, p.content, p.ts, p.pinned, p.like_people, p.retweet_people, p.post_id, u.like_ts FROM post_records AS p, user_likes AS u WHERE u.username = $1 AND p.post_id = u.post_id ORDER BY u.like_ts DESC';
 
+//user_retweet
+const userRetweet = 'INSERT INTO user_retweets(username, post_id) VALUES($1, $2)';
+const delUserRetweet = 'DELETE FROM user_retweets WHERE username = $1 AND post_id = $2';
+const fetchUserRetweet = 'SELECT p.postby, p.content, p.ts, p.pinned, p.like_people, p.retweet_people, p.post_id, u.retweet_ts FROM post_records AS p, user_retweets AS u WHERE u.username = $1 AND p.post_id = u.post_id ORDER BY u.retweet_ts DESC';
+
 module.exports = {
+    //regist
     regist,
     findDup,
     findOne,
+
+    //post
     postData,
     fetchPost,
     newPostId,
     insertLikePeople,
     removeLikePeople,
+    insertRetweetPeople,
+    removeRetweetPeople,
+
+    //user_like
     userLike,
     delUserLike,
     fetchUserLike,
+
+    //user_retweet
+    userRetweet,
+    delUserRetweet,
+    fetchUserRetweet,
 };
