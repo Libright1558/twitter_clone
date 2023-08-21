af50e5d48f617c2453d5328b78c4208bf7306854
libright1558 <n32717325@gmail.com>
2023-08-21 21:22:40 +0800

    feat: rewrite major functions......

--- redis/utility.js
+++ redis/utility.js
@@ -1,25 +1,65 @@
-const fetchPostDetail = async (recordsObj, client, username) => {
-    const recordLen = recordsObj.length;
-    for(let i = 0; i < recordLen; i++) {
-        const likenum = JSON.parse(await client.hGet(username + "_likenum", recordsObj[i].post_id));
-        const retweetnum = JSON.parse(await client.hGet(username + "_retweetnum", recordsObj[i].post_id));
-        const isliked = JSON.parse(await client.hGet(username + "_isliked", recordsObj[i].post_id));
-        const isretweeted = JSON.parse(await client.hGet(username + "_isretweeted", recordsObj[i].post_id));
+const controller = require("../controller");
 
-        recordsObj[i].likenum = likenum;
-        recordsObj[i].retweetnum = retweetnum;
-        recordsObj[i].isliked = isliked;
-        recordsObj[i].isretweeted = isretweeted;
+const fetchPostDetail = async (recordsObj, recordsObjIdArray, client, username) => {
+    const recordLen = recordsObj.length;
+
+    const [likenum, retweetnum, isliked, isretweeted] = await client
+        .multi()
+        .HMGET("likenum", recordsObjIdArray)
+        .HMGET("retweetnum", recordsObjIdArray)
+        .HMGET(username + "_isliked", recordsObjIdArray)
+        .HMGET(username + "_isretweeted", recordsObjIdArray)
+        .exec();
+
+    // if likenum or isliked doesn't exist, fetching it from database. then writing it back to redis
+    if(!likenum || !isliked) {
+        const response = await controller.fetchPostLikeDetail(recordsObjIdArray, username);
+        const likeNumObj = {};
+        const isLikeObj = {};
+
+        for(let i = 0; i < recordLen; i++) {
+            likeNumObj[recordsObjIdArray[i]] = response.rows[i].likenum;
+            isLikeObj[recordsObjIdArray[i]] = response.rows[i].isliked;
+
+            recordsObj[i].likenum = JSON.parse(response.rows[i].likenum);
+            recordsObj[i].isliked = JSON.parse(response.rows[i].isliked);
+        }
+
+        await client
+        .multi()
+        .HSET("likenum", likeNumObj)
+        .HSET(username + "_isliked", isLikeObj)
+        .exec();
     }
 
+    // if retweetnum or isretweeted doesn't exist, fetching it from database. then writing it back to redis
+    if(!retweetnum || !isretweeted) {
+
+    }
+    
+    for(let i = 0; i < recordLen; i++) {
+        if((!likenum || !isliked) && (!retweetnum || !isretweeted)) {
+            break;
+        }
+        if(likenum && isliked) {
+            recordsObj[i].likenum = JSON.parse(likenum[i]);
+            recordsObj[i].isliked = JSON.parse(isliked[i]);
+        }
+
+        if(retweetnum && isretweeted) {
+            recordsObj[i].retweetnum = JSON.parse(retweetnum[i]);
+            recordsObj[i].isretweeted = JSON.parse(isretweeted[i]);
+        }
+    }    
+    
     return recordsObj;
 }
 
 const setCacheExp = async (username, redis_cache) => {
-    await redis_cache.setExp(username + "_likenum", process.env.exp_time);
-    await redis_cache.setExp(username + "_retweetnum", process.env.exp_time);
-    await redis_cache.setExp(username + "_isretweeted", process.env.exp_time);
-    await redis_cache.setExp(username + "_isliked", process.env.exp_time);
+    await redis_cache.setExpNX("likenum", process.env.exp_time);
+    await redis_cache.setExpNX("retweetnum", process.env.exp_time);
+    await redis_cache.setExpNX(username + "_isretweeted", process.env.exp_time);
+    await redis_cache.setExpNX(username + "_isliked", process.env.exp_time);
 }
 
 module.exports = {
