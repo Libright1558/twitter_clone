a6ac87e29a6160a8974acfed893c92c908ee0a81
libright1558 <n32717325@gmail.com>
2023-09-08 21:45:29 +0800

    change commonJS to ES6

--- database/database.js
+++ database/database.js
@@ -1,11 +1,11 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+import pg from 'pg';
+import 'dotenv/config';
 
-const pool = new Pool({
+const pool = new pg.Pool({
     host: process.env.db_host,
     database: process.env.db_schema,
     port: process.env.db_port,
     user: process.env.db_user,
 });
 
-module.exports = pool;
+export default pool;
