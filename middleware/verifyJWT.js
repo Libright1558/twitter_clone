353aa6d8f1d3b929d872b0a8f05eaa1636af54ac
libright1558 <n32717325@gmail.com>
2023-09-08 21:40:16 +0800

    add verifyJWT to authenticate users

---
+++ middleware/verifyJWT.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
+import fs from 'fs';
+
+const verifyJWT = (req, res, next) => {
+    const cookies = req.cookies;
+    if(!cookies?.access) return res.sendStatus(401);
+    const token = cookies.access;
+
+    const pubkey = fs.readFileSync(process.env.public_key, 'utf8');
+    jwt.verify(token, pubkey, { algorithms: ['RS256'] }, (err, decoded) => {
+        if(err && err.message === 'jwt expired') {
+            return res.status(403).send(err);// token expired
+        }
+
+        if(err || !(decoded.username)) {
+            return res.sendStatus(403);// invalid token
+        }
+        
+        req.username = decoded.username;
+        next();
+    })
+}
+
+export default verifyJWT
