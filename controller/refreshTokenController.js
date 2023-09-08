2ce39426f92cd8b76815f4627822c7449e10919f
libright1558 <n32717325@gmail.com>
2023-09-08 21:48:46 +0800

    To gain the new access token

---
+++ controller/refreshTokenController.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import fs from 'fs';
+import 'dotenv/config';
+
+const handleRefreshToken = async (req, res, next) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(401);
+    const refreshToken = cookies.jwt;
+
+    const pubkey = fs.readFileSync(process.env.public_key, 'utf8');
+    jwt.verify(refreshToken, pubkey, { algorithms: ['RS256'] }, (err, decoded) => {
+        if (err) return res.sendStatus(403);
+
+        const prikey = fs.readFileSync(process.env.private_key, 'utf8');
+        const accessToken = jwt.sign(
+            { "username": decoded.username },
+            prikey,
+            { algorithm: 'RS256', expiresIn: '30s' }
+        );
+        res.cookie('access', accessToken, { httpOnly: true, sameSite: true, maxAge: 15 * 60 * 1000});
+        return res.end();
+    });
+}
+
+export default {
+    handleRefreshToken,
+}
