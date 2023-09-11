d05ddbad48068fa67b7080b68e36f3deb728d138
libright1558 <n32717325@gmail.com>
2023-09-11 16:15:15 +0800

    rewrite the fetch function to refetch the resource

--- public/js/home.js
+++ public/js/home.js
@@ -1,64 +1,62 @@
 import fetchNewAccessToken from "./library/getNewAccessToken.js";
 import common from "./common.js";
+import fetchRetry from "./library/fetchRetry.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("/api/posts", {
+    fetchRetry("/api/posts", 500, 2, {
         credentials: "include",
         method: 'GET'
-    })
-    .then((response) => {
-        return response.json();
-    })
-    .then(async (results) => {
-        if(results.message === 'jwt expired') {
-            await fetchNewAccessToken();
-        }
+    }, handlePostManage);
+});
 
-        const postsContainer = document.querySelector('.postsContainer');
 
-        let resultPost = {
-            "firstName": null,
-            "lastName": null,
-            "profilePic": null,
-            "timestamp": null,
-            "postData": null,
-            "postby": null,
-            "post_id": null,
-            "likenum": null,
-            "retweetnum": null,
-            "isretweeted": null,
-            "isliked": null
-        }
+async function handlePostManage(results) {
+    if(results.message === 'jwt expired') {
+        await fetchNewAccessToken();
+        throw new Error('token expired!');
+    }
 
-        const userPostsLength = results.userPosts ? results.userPosts.length : 0;
-        const userRetweetsLength = results.userRetweets ? results.userRetweets.length : 0;
+    const postsContainer = document.querySelector('.postsContainer');
 
-        let i = 0, j = 0;
-        while(i < userPostsLength && j < userRetweetsLength) {
-            if(results.userPosts[i].ts > results.userRetweets[j].retweet_ts) {
-                await renderPost(resultPost, results.userPosts[i], postsContainer);
-                i++;
-            }
-            else {
-                await renderPost(resultPost, results.userRetweets[j], postsContainer);
-                j++;
-            }
-        }
+    let resultPost = {
+        "firstName": null,
+        "lastName": null,
+        "profilePic": null,
+        "timestamp": null,
+        "postData": null,
+        "postby": null,
+        "post_id": null,
+        "likenum": null,
+        "retweetnum": null,
+        "isretweeted": null,
+        "isliked": null
+    }
 
-        while(i < userPostsLength) {
+    const userPostsLength = results.userPosts ? results.userPosts.length : 0;
+    const userRetweetsLength = results.userRetweets ? results.userRetweets.length : 0;
+
+    let i = 0, j = 0;
+    while(i < userPostsLength && j < userRetweetsLength) {
+        if(results.userPosts[i].ts > results.userRetweets[j].retweet_ts) {
             await renderPost(resultPost, results.userPosts[i], postsContainer);
             i++;
         }
-
-        while(j < userRetweetsLength) {
+        else {
             await renderPost(resultPost, results.userRetweets[j], postsContainer);
             j++;
         }
-    })
-    .catch(error => {
-        console.log("render posts error", error);
-    })
-});
+    }
+
+    while(i < userPostsLength) {
+        await renderPost(resultPost, results.userPosts[i], postsContainer);
+        i++;
+    }
+
+    while(j < userRetweetsLength) {
+        await renderPost(resultPost, results.userRetweets[j], postsContainer);
+        j++;
+    }
+}
 
 async function renderPost(resultPost, result, element) {
     resultPost.firstName = result.firstname
