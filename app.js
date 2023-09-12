7a3c51cd72403226ec284194de7ab515bbc35a67
libright1558 <n32717325@gmail.com>
2023-09-12 16:29:51 +0800

    change the format

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-import express from 'express';
-const app = express();
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-import 'dotenv/config';
-import verifyJWT from "./middleware/verifyJWT.js";
-import cookieParser from 'cookie-parser';
-const port = process.env.port;
+import express from 'express'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import 'dotenv/config'
+import verifyJWT from './middleware/verifyJWT.js'
+import cookieParser from 'cookie-parser'
 
-const server = app.listen(port, () => console.log("server listening on port " + port));
+// Routes
+import loginRoute from './routes/loginRoutes.js'
+import registerRoute from './routes/registerRoutes.js'
+import logoutRoute from './routes/logoutRoutes.js'
+import refreshRoute from './routes/refreshTokenRoutes.js'
+import root from './routes/root.js'
 
-app.set("view engine", "pug");
-app.set("views", "views");
+import postsApiRoute from './routes/api/posts.js' // API Routes
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.static(join(__dirname, "public")));
-app.use(cookieParser());
+const app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const port = process.env.port
 
+app.listen(port, () => console.log('server listening on port ' + port))
 
-//Routes
-import loginRoute from './routes/loginRoutes.js';
-import registerRoute from './routes/registerRoutes.js';
-import logoutRoute from './routes/logoutRoutes.js';
-import refreshRoute from './routes/refreshTokenRoutes.js';
-import root from './routes/root.js';
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(join(__dirname, 'public')))
+app.use(cookieParser())
 
-import postsApiRoute from './routes/api/posts.js'; //API Routes
+app.use('/login', loginRoute)
+app.use('/register', registerRoute)
+app.use('/logout', logoutRoute)
+app.use('/refresh', refreshRoute)
+app.use('/', root)
 
-app.use("/login", loginRoute);
-app.use("/register", registerRoute);
-app.use("/logout", logoutRoute);
-app.use("/refresh", refreshRoute);
-app.use("/", root);
-
-app.use(verifyJWT);
-app.use("/api/posts", postsApiRoute);
+app.use(verifyJWT)
+app.use('/api/posts', postsApiRoute)
