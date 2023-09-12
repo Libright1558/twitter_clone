1bc789fb123c25834e59e251b54a7c12f0ee8877
libright1558 <n32717325@gmail.com>
2023-09-12 16:30:16 +0800

    change the format

--- controller/rootController.js
+++ controller/rootController.js
@@ -1,34 +1,31 @@
-import databaseController from "./databaseController.js";
-import redis_cache from "../redis/cache.js";
+import databaseController from './databaseController.js'
+import redis_cache from '../redis/cache.js'
 
 const intoTheHomePage = async (req, res, next) => {
-    try {
-        const username = req.username;
+  try {
+    const username = req.username
 
-        let personalData = await redis_cache.fetchPersonalData(username);
-        if(!personalData) {
-            const response = await databaseController.fetchPersonalData(username);
-            personalData = response.rows[0];
-            await redis_cache.writePersonalData(username, personalData);
-            await redis_cache.setExpNX("personalData", 900);
-        }
-        else {
-            personalData = JSON.parse(personalData);
-        }
-        
-        const payload = {
-            pageTitle: "Home",
-            UserloggedIn: personalData,
-            UserloggedInJs: JSON.stringify(personalData),
-        }
-        res.status(200).render("home", payload);
-    
+    let personalData = await redis_cache.fetchPersonalData(username)
+    if (!personalData) {
+      const response = await databaseController.fetchPersonalData(username)
+      personalData = response.rows[0]
+      await redis_cache.writePersonalData(username, personalData)
+      await redis_cache.setExpNX('personalData', 900)
+    } else {
+      personalData = JSON.parse(personalData)
     }
-    catch (err) {
-        console.log("intoTheHomePage error", err);
+
+    const payload = {
+      pageTitle: 'Home',
+      UserloggedIn: personalData,
+      UserloggedInJs: JSON.stringify(personalData)
     }
+    res.status(200).render('home', payload)
+  } catch (err) {
+    console.log('intoTheHomePage error', err)
+  }
 }
 
 export default {
-    intoTheHomePage,
+  intoTheHomePage
 }
