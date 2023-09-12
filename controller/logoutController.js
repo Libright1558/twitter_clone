735781f8186bb9a0f8e97bbbefaa782b724a768e
libright1558 <n32717325@gmail.com>
2023-09-12 16:30:03 +0800

    change the format

--- controller/logoutController.js
+++ controller/logoutController.js
@@ -1,15 +1,15 @@
 const logout = async (req, res, next) => {
-    const cookies = req.cookies;
-    if (!cookies?.jwt) {
-        if(cookies.access) res.clearCookie('access', { httpOnly: true, sameSite: true, maxAge: 15 * 60 * 1000 });
-        return res.redirect("./login");
-    }
+  const cookies = req.cookies
+  if (!cookies?.jwt) {
+    if (cookies.access) res.clearCookie('access', { httpOnly: true, sameSite: true, maxAge: 15 * 60 * 1000 })
+    return res.redirect('./login')
+  }
 
-    res.clearCookie('jwt', { httpOnly: true, sameSite: true, maxAge: 24 * 60 * 60 * 1000 });
-    res.clearCookie('access', { httpOnly: true, sameSite: true, maxAge: 15 * 60 * 1000 });
-    return res.redirect("./login");
+  res.clearCookie('jwt', { httpOnly: true, sameSite: true, maxAge: 24 * 60 * 60 * 1000 })
+  res.clearCookie('access', { httpOnly: true, sameSite: true, maxAge: 15 * 60 * 1000 })
+  return res.redirect('./login')
 }
 
 export default {
-    logout,
+  logout
 }
