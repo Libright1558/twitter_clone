aa24f2de794887cd34387e9a275ed4722aa0bf21
libright1558 <n32717325@gmail.com>
2023-09-12 16:30:19 +0800

    change the format

--- database/database.js
+++ database/database.js
@@ -1,11 +1,11 @@
-import pg from 'pg';
-import 'dotenv/config';
+import pg from 'pg'
+import 'dotenv/config'
 
 const pool = new pg.Pool({
-    host: process.env.db_host,
-    database: process.env.db_schema,
-    port: process.env.db_port,
-    user: process.env.db_user,
-});
+  host: process.env.db_host,
+  database: process.env.db_schema,
+  port: process.env.db_port,
+  user: process.env.db_user
+})
 
-export default pool;
+export default pool
