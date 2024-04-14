82f417cb686900356ab662c4b579b8359fc25474
libright1558 <n32717325@gmail.com>
2024-04-14 22:27:18 +0800

    Change the function name to CamelCase style
    As the title says.

--- controller/rootController.js
+++ controller/rootController.js
@@ -1,16 +1,16 @@
 import databaseController from './databaseController.js'
-import redis_cache from '../redis/cache.js'
+import redisCache from '../redis/cache.js'
 
 const intoTheHomePage = async (req, res, next) => {
   try {
     const username = req.username
 
-    let personalData = await redis_cache.fetchPersonalData(username)
+    let personalData = await redisCache.fetchPersonalData(username)
     if (!personalData) {
       const response = await databaseController.fetchPersonalData(username)
       personalData = response.rows[0]
-      await redis_cache.writePersonalData(username, personalData)
-      await redis_cache.setExpNX('personalData', 900)
+      await redisCache.writePersonalData(username, personalData)
+      await redisCache.setExpNX('personalData', 900)
     } else {
       personalData = JSON.parse(personalData)
     }
