7868d4b5dc0d4960bfc225c6ce3e51626b82187f
libright1558 <n32717325@gmail.com>
2024-05-20 00:03:28 +0800

    Add double quotes to prevent the error

--- database/queryString/queryUser.js
+++ database/queryString/queryUser.js
@@ -1,6 +1,6 @@
 const personalData =
-`SELECT firstname, lastname, username, email, profilepic FROM user 
-WHERE userId = $1`
+`SELECT firstname, lastname, username, email, profilepic FROM user_table 
+WHERE "userId" = $1`
 
 export default {
   personalData
