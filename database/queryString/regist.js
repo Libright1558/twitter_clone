a8b6aa09b870d36c47806312ad82c28f7028e27a
libright1558 <n32717325@gmail.com>
2024-05-20 00:01:39 +0800

    Add findDup and findOne query string

--- database/queryString/regist.js
+++ database/queryString/regist.js
@@ -1,7 +1,17 @@
 const regist =
-`INSERT INTO user(firstname, lastname, username, email, password, profilepic) 
+`INSERT INTO user_table(firstname, lastname, username, email, password, profilepic) 
 VALUES($1, $2, $3, $4, $5, $6)`
 
+const findDup =
+`SELECT username, email FROM user_table
+WHERE username = $1 OR email = $2`
+
+const findOne =
+`SELECT "userId", username, password FROM user_table
+WHERE username = $1 OR email = $1`
+
 export default {
-  regist
+  regist,
+  findDup,
+  findOne
 }
