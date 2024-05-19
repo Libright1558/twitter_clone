9a9df57c3a8a153fdaac3b7be1c5aa539a26b14d
libright1558 <n32717325@gmail.com>
2024-05-20 00:05:05 +0800

    Add query strings

--- database/queryString/deletePost.js
+++ database/queryString/deletePost.js
@@ -1,7 +1,13 @@
 const deletePost =
-`DELETE FROM post_records 
-WHERE post_id = $1`
+`DELETE FROM post_table 
+WHERE "postId" = $1`
+
+const deleteLike = 'DELETE FROM like_table WHERE "postId" = $1 AND username = $2'
+
+const deleteRetweet = 'DELETE FROM retweet_table WHERE "postId" = $1 AND username = $2'
 
 export default {
-  deletePost
+  deletePost,
+  deleteLike,
+  deleteRetweet
 }
