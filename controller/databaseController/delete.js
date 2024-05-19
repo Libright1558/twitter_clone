e798ff425591b20ea2e0d9ba4dd4abbfee2a61dc
libright1558 <n32717325@gmail.com>
2024-05-20 00:01:11 +0800

    Implement removeLIke and removeRetweet

--- controller/databaseController/delete.js
+++ controller/databaseController/delete.js
@@ -1,5 +1,7 @@
 import sequelize from '../../database/sequelize.js'
+import pool from '../../database/pgPool.js'
 import deleteString from '../../database/queryString/deletePost.js'
+import queryPost from '../../database/queryString/queryPost.js'
 import { QueryTypes } from 'sequelize'
 
 const deletePost = async (postId) => {
@@ -13,6 +15,70 @@ const deletePost = async (postId) => {
   }
 }
 
+const removeLike = async (param) => {
+  try {
+    const client = await pool.connect()
+    try {
+      await client.query('BEGIN')
+      await client.query(deleteString.deleteLike, [param.postId, param.username])
+      const deletedAt = await client.query('SELECT NOW() AS "deletedAt"')
+
+      await client.query(queryPost.postData)
+      await client.query(queryPost.likeNum)
+
+      await client.query(queryPost.appendPostId, [param.username])
+      await client.query(queryPost.appendLikeNum)
+      const likeNumsInfo = await client.query(queryPost.fetchLikeNum, [param.postId])
+      await client.query('COMMIT')
+
+      const result = {
+        likeNumsInfo: likeNumsInfo.rows,
+        deletedAt: deletedAt.rows
+      }
+      return result
+    } catch (error) {
+      await client.query('ROLLBACK')
+    } finally {
+      client.release()
+    }
+  } catch (error) {
+    console.log('pgPool connection error: ', error)
+  }
+}
+
+const removeRetweet = async (param) => {
+  try {
+    const client = await pool.connect()
+    try {
+      await client.query('BEGIN')
+      await client.query(deleteString.deleteRetweet, [param.postId, param.username])
+      const deletedAt = await client.query('SELECT NOW() AS "deletedAt"')
+
+      await client.query(queryPost.postData)
+      await client.query(queryPost.retweetNum)
+
+      await client.query(queryPost.appendPostId, [param.username])
+      await client.query(queryPost.appendRetweetNum)
+      const retweetNumsInfo = await client.query(queryPost.fetchRetweetNum, [param.postId])
+      await client.query('COMMIT')
+
+      const result = {
+        retweetNumsInfo: retweetNumsInfo.rows,
+        deletedAt: deletedAt.rows
+      }
+      return result
+    } catch (error) {
+      await client.query('ROLLBACK')
+    } finally {
+      client.release()
+    }
+  } catch (error) {
+    console.log('pgPool connection error: ', error)
+  }
+}
+
 export {
-  deletePost
+  deletePost,
+  removeLike,
+  removeRetweet
 }
