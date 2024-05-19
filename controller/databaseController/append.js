0e085504a13237ec8eb63eff485da13deaf95561
libright1558 <n32717325@gmail.com>
2024-05-20 00:00:38 +0800

    Implement appendLike and appendRetweet

--- controller/databaseController/append.js
+++ controller/databaseController/append.js
@@ -1,19 +1,107 @@
 import sequelize from '../../database/sequelize.js'
+import pool from '../../database/pgPool.js'
 import appendPost from '../../database/queryString/appendPost.js'
+import queryPost from '../../database/queryString/queryPost.js'
 import { QueryTypes } from 'sequelize'
 
+/*
+* postData =
+* {
+*  postby,
+*  content
+* }
+*/
 const insertPost = async (postData) => {
   try {
-    const postId = await sequelize.query(appendPost.appendPostData, {
-      bind: [postData],
+    const [postId, metadata] = await sequelize.query(appendPost.appendPostData, {
+      bind: [postData.postby, postData.content],
       type: QueryTypes.INSERT
     })
-    return postId
+    const result = [postId, metadata]
+    return result
   } catch (err) {
     console.log('appendPost error: ', err)
   }
 }
 
+/*
+* param =
+* {
+*  postId,
+*  username
+* }
+*/
+const appendLike = async (param) => {
+  try {
+    const client = await pool.connect()
+    try {
+      await client.query('BEGIN')
+      const createdAt = await client.query(appendPost.addLike, [param.postId, param.username])
+
+      await client.query(queryPost.postData)
+      await client.query(queryPost.likeNum)
+
+      await client.query(queryPost.appendPostId, [param.username])
+      await client.query(queryPost.appendLikeNum)
+      const likeNumsInfo = await client.query(queryPost.fetchLikeNum, [param.postId])
+      await client.query('COMMIT')
+
+      const result = {
+        likeNumsInfo: likeNumsInfo.rows,
+        createdAt: createdAt.rows
+      }
+      return result
+    } catch (error) {
+      await client.query('ROLLBACK')
+      console.log('appendLike error: ', error)
+    } finally {
+      client.release()
+    }
+  } catch (error) {
+    console.log('pgPool connection error: ', error)
+  }
+}
+
+/*
+* param =
+* {
+*  postId,
+*  username
+* }
+*/
+const appendRetweet = async (param) => {
+  try {
+    const client = await pool.connect()
+    try {
+      await client.query('BEGIN')
+      const createdAt = await client.query(appendPost.addRetweet, [param.postId, param.username])
+
+      await client.query(queryPost.postData)
+      await client.query(queryPost.retweetNum)
+
+      await client.query(queryPost.appendPostId, [param.username])
+      await client.query(queryPost.appendRetweetNum)
+      const retweetNumsInfo = await client.query(queryPost.fetchRetweetNum, [param.postId])
+      await client.query('COMMIT')
+
+      const result = {
+        retweetNumsInfo: retweetNumsInfo.rows,
+        createdAt: createdAt.rows
+      }
+      return result
+    } catch (error) {
+      await client.query('ROLLBACK')
+      console.log('appendRetweet error: ', error)
+    } finally {
+      client.release()
+    }
+  } catch (error) {
+    console.log('pgPool connection error: ', error)
+  }
+}
+
 export {
-  insertPost
+  insertPost,
+  appendLike,
+  appendRetweet
 }
