05b5da9187f198690eb9aa1053d56807f5d46ff1
libright1558 <n32717325@gmail.com>
2024-05-28 00:03:24 +0800

    Minor changes

--- public/js/home.js
+++ public/js/home.js
@@ -1,20 +1,22 @@
-import fetchNewAccessToken from './library/getNewAccessToken.js'
 import common from './common.js'
-import fetchRetry from './library/fetchRetry.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetchRetry('/api/posts', 500, 2, {
+  fetch('/api/posts', {
     credentials: 'include',
     method: 'GET'
-  }, handlePostManage)
+  })
+    .then((results) => {
+      return results.json()
+    })
+    .then((returnedValue) => {
+      handlePostManage(returnedValue)
+    })
+    .catch(e => {
+      console.log('get posts fail: ', e)
+    })
 })
 
 async function handlePostManage (results) {
-  if (results.message === 'jwt expired') {
-    await fetchNewAccessToken()
-    throw new Error('token expired!')
-  }
-
   const postsContainer = document.querySelector('.postsContainer')
 
   const resultPost = {
@@ -61,15 +63,19 @@ async function renderPost (resultPost, result, element) {
   resultPost.firstName = result.firstname
   resultPost.lastName = result.lastname
   resultPost.profilePic = result.profilepic
-  resultPost.timestamp = result.ts
+  resultPost.timestamp = result.createdAt
   resultPost.postData = result.content
-  resultPost.post_id = result.post_id
+  resultPost.post_id = result.postId
   resultPost.postby = result.postby
-  resultPost.likenum = result.likenum
-  resultPost.retweetnum = result.retweetnum
-  resultPost.isretweeted = result.isretweeted
-  resultPost.isliked = result.isliked
+  resultPost.likenum = parseInt(result.likeNums)
+  resultPost.retweetnum = parseInt(result.retweetNums)
+  resultPost.isretweeted = parseInt(result.selfRetweet) === 1
+  resultPost.isliked = parseInt(result.selfLike) === 1
 
   const html = common.createPostHtml(resultPost)
   element.insertAdjacentHTML('beforeend', html)
+}
+
+export {
+  renderPost
 }
