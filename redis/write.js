e72cf0a27558f644f5997528b50eecbaae8e1a68
libright1558 <n32717325@gmail.com>
2024-05-29 00:06:38 +0800

    Remove unnecessary connections

--- redis/write.js
+++ redis/write.js
@@ -1,40 +1,28 @@
-import { client } from './cache.js'
+import { client } from './init.js'
 import { writePostInfo, writeUserInfo, writePostIdArray } from './transaction.js'
 
 const writePersonalData = async (userId, userInfo) => {
-  await client.connect()
-
   try {
     await writeUserInfo(client, userId, userInfo)
   } catch (err) {
     console.log('redis writePersonalData error', err)
-  } finally {
-    await client.quit()
   }
 }
 
 const postWriteBack = async (userId, postInfoObj, listArray) => {
-  await client.connect()
-
   try {
     await writePostInfo(client, userId, postInfoObj, listArray)
   } catch (err) {
     console.log('redis postWriteBack error', err)
-  } finally {
-    await client.quit()
   }
 }
 
 const setPostIdArray = async (userId, member) => {
-  await client.connect()
-
   try {
     const result = await writePostIdArray(client, userId, member)
     return result
   } catch (err) {
     console.log('redis writeSortedPostIdArray error', err)
-  } finally {
-    await client.quit()
   }
 }
 
