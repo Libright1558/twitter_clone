c304f01ff48d510d1593ebd625aac7f443c3e7d6
libright1558 <n32717325@gmail.com>
2024-05-29 00:07:08 +0800

    Remove unnecessary connections

--- redis/cache.js
+++ redis/cache.js
@@ -1,70 +1,47 @@
-import * as redis from 'redis'
+import { client } from './init.js'
 import { setUserInfoExp, setPostInfoExp } from './transaction.js'
-const client = redis.createClient()
-
-client.on('error', err => console.log('Redis Client Error', err))
 
 // test if connection is alive
 const pingTest = async () => {
-  await client.connect()
-
   try {
     const response = await client.PING()
     return response
   } catch (err) {
     console.log('redis pingTest error', err)
-  } finally {
-    await client.quit()
   }
 }
 
 // set expire
 const setUserExpNX = async (times) => {
-  await client.connect()
-
   try {
     await setUserInfoExp(client, times)
   } catch (err) {
     console.log('redis setUserExpNX error', err)
-  } finally {
-    await client.quit()
   }
 }
 
 const setPostExpNX = async (userId, times) => {
-  await client.connect()
-
   try {
     await setPostInfoExp(client, userId, times)
   } catch (err) {
     console.log('redis setPostExpNX error', err)
-  } finally {
-    await client.quit()
   }
 }
 
 // delete key
 const delKey = async (key) => {
-  await client.connect()
-
   try {
     await client.DEL(key)
   } catch (err) {
     console.log('redis delKey error', err)
-  } finally {
-    await client.quit()
   }
 }
 
 const delHashField = async (key, field) => {
-  await client.connect()
-
   try {
     await client.HDEL(key, field)
   } catch (err) {
     console.log('redis delField error', err)
-  } finally {
-    await client.quit()
   }
 }
 
