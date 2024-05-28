52f8cace0687400eda3928b67ccf7c0cd5facf90
libright1558 <n32717325@gmail.com>
2024-05-29 00:06:53 +0800

    Remove unnecessary connections

--- redis/get.js
+++ redis/get.js
@@ -1,42 +1,30 @@
-import { client } from './cache.js'
+import { client } from './init.js'
 import { getPostInfo, getUserInfo, getPostIdArray } from './transaction.js'
 
 const fetchPersonalData = async (userId) => {
-  await client.connect()
-
   try {
     const result = await getUserInfo(client, userId)
     return result
   } catch (err) {
     console.log('redis fetchPersonalData error', err)
-  } finally {
-    await client.quit()
   }
 }
 
 const getPosts = async (userId, postIdArray) => {
-  await client.connect()
-
   try {
     const result = await getPostInfo(client, userId, postIdArray)
     return result
   } catch (err) {
     console.log('redis getPosts error', err)
-  } finally {
-    await client.quit()
   }
 }
 
 const fetchPostIdArray = async (userId) => {
-  await client.connect()
-
   try {
     const result = await getPostIdArray(client, userId)
     return result
   } catch (err) {
     console.log('redis getSortedPostIdArray error', err)
-  } finally {
-    await client.quit()
   }
 }
 
