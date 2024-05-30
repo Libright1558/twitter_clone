dccdb7c3944055e2b053348143a239234f0f7221
libright1558 <n32717325@gmail.com>
2024-05-30 20:52:01 +0800

    Add the connection succeed message

--- redis/init.js
+++ redis/init.js
@@ -2,7 +2,8 @@ import { createClient } from 'redis'
 
 const client = createClient()
 client.on('error', err => console.log('Redis Client Error', err))
-await client.connect()
+client.connect()
+  .then(() => { console.log('redis connection succeed') })
 
 export {
   client
