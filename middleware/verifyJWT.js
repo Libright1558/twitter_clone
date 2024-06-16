1b58a99f12967788b3c2653e66bdae7c366bf838
libright1558 <n32717325@gmail.com>
2024-06-16 16:02:29 +0800

    Use eslint to change the format of codes

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,26 +1,26 @@
-import jwt from 'jsonwebtoken'
-import 'dotenv/config'
-import fs from 'fs'
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+import fs from 'fs';
 
 const verifyJWT = (req, res, next) => {
-  const cookies = req.cookies
-  if (!cookies?.access) return res.sendStatus(401)
-  const token = cookies.access
+    const cookies = req.cookies;
+    if (!cookies?.access) return res.sendStatus(401);
+    const token = cookies.access;
 
-  const pubkey = fs.readFileSync(process.env.public_key, 'utf8')
-  jwt.verify(token, pubkey, { algorithms: ['RS256'] }, (err, decoded) => {
-    if (err && err.message === 'jwt expired') {
-      return res.status(403).send(err)// token expired
-    }
+    const pubkey = fs.readFileSync(process.env.public_key, 'utf8');
+    jwt.verify(token, pubkey, { algorithms: ['RS256'] }, (err, decoded) => {
+        if (err && err.message === 'jwt expired') {
+            return res.status(403).send(err);// token expired
+        }
 
-    if (err || !(decoded.userId) || !(decoded.username)) {
-      return res.sendStatus(403)// invalid token
-    }
+        if (err || !(decoded.userId) || !(decoded.username)) {
+            return res.sendStatus(403);// invalid token
+        }
 
-    req.userId = decoded.userId
-    req.username = decoded.username
-    next()
-  })
-}
+        req.userId = decoded.userId;
+        req.username = decoded.username;
+        next();
+    });
+};
 
-export default verifyJWT
+export default verifyJWT;
