7b993736bf72926aac01cf0d664cef741700a33b
libright1558 <n32717325@gmail.com>
2024-07-22 23:43:10 +0800

    Add status codes and some changes

--- src/controller/refreshTokenController.ts
+++ src/controller/refreshTokenController.ts
@@ -1,31 +1,30 @@
 import jwt from 'jsonwebtoken';
 import fs from 'fs';
 import 'dotenv/config';
-import { ExpressReq, ExpressRes, DecodedValue } from '..';
+import { Request, Response, DecodedValue } from '..';
 
-const handleRefreshToken = async (req: ExpressReq, res: ExpressRes) => {
+const handleRefreshToken = async (req: Request, res: Response) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
 
     const pubkey = fs.readFileSync(`${process.env.public_key}`, 'utf8');
-    jwt.verify(refreshToken, pubkey, { algorithms: ['RS256'] }, (err, decoded) => {
+    jwt.verify(refreshToken, pubkey, { algorithms: ['RS256'] }, (err, decoded: DecodedValue) => {
         if (err) return res.sendStatus(403);
 
         const prikey = fs.readFileSync(`${process.env.private_key}`, 'utf8');
         if (decoded) {
-            decoded = decoded as DecodedValue;
             const userId = decoded.userId;
             const username = decoded.username;
             const accessToken = jwt.sign(
                 { userId, username },
                 prikey,
-                { algorithm: 'RS256', expiresIn: '300s' }
+                { algorithm: 'RS256', expiresIn: '60s' }
             );
-            res.cookie('access', accessToken, { httpOnly: true, sameSite: true, maxAge: 15 * 60 * 1000 });
+            res.cookie('access', accessToken, { httpOnly: true, sameSite: true, maxAge: 2 * 60 * 1000 });
         }
         
-        return res.end();
+        return res.status(200).end();
     });
 };
 
