7367f07bf995a7aa9179b8158a7d15a59f8d0b78
libright1558 <n32717325@gmail.com>
2024-07-22 23:41:04 +0800

    Add status codes

--- src/controller/logoutController.ts
+++ src/controller/logoutController.ts
@@ -1,15 +1,15 @@
-import { ExpressReq, ExpressRes } from '..';
+import { Request, Response } from '..';
 
-const logout = async (req: ExpressReq, res: ExpressRes) => {
+const logout = async (req: Request, res: Response) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) {
         if (cookies.access) res.clearCookie('access', { httpOnly: true, sameSite: true, maxAge: 15 * 60 * 1000 });
-        return res.redirect('./login');
+        return res.redirect(401, './login');
     }
 
-    res.clearCookie('jwt', { httpOnly: true, sameSite: true, maxAge: 24 * 60 * 60 * 1000 });
-    res.clearCookie('access', { httpOnly: true, sameSite: true, maxAge: 15 * 60 * 1000 });
-    return res.redirect('./login');
+    res.clearCookie('jwt', { httpOnly: true, sameSite: true, maxAge: 7 * 24 * 60 * 60 * 1000 });
+    res.clearCookie('access', { httpOnly: true, sameSite: true, maxAge: 2 * 60 * 1000 });
+    return res.redirect(200, './login');
 };
 
 export default {
