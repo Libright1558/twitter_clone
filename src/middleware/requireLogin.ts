42e7d43b3797933ce8f45df42e03afef10fd0a0d
libright1558 <n32717325@gmail.com>
2024-07-22 23:44:38 +0800

    Sign the user identifiers into headers

--- src/middleware/requireLogin.ts
+++ src/middleware/requireLogin.ts
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
 import fs from 'fs';
 import 'dotenv/config';
-import { ReqValue, ExpressRes, ExpressNext, DecodedValue } from '..';
+import { Request, Response, NextFunction, DecodedValue } from '..';
 
-const preLogin = (req: ReqValue, res: ExpressRes, next: ExpressNext) => {
+const preLogin = (req: Request, res: Response, next: NextFunction) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.redirect('/login');
     const refreshToken = cookies.jwt;
@@ -11,8 +11,9 @@ const preLogin = (req: ReqValue, res: ExpressRes, next: ExpressNext) => {
     const pubkey = fs.readFileSync(process.env.public_key!, 'utf8');
     jwt.verify(refreshToken, pubkey, { algorithms: ['RS256'] }, (err, decoded: DecodedValue) => {
         if (err || !(decoded.userId) || !(decoded.username)) return res.redirect('/login');
-        req.userId = decoded.userId;
-        req.username = decoded.username;
+        
+        req.headers['userId'] = decoded.userId;
+        req.headers['username'] = decoded.username;
         next();
     });
 };
