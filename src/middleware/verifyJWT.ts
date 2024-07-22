655bce847fbc46c897a377be1a43c4262cbaab7f
libright1558 <n32717325@gmail.com>
2024-07-22 23:44:59 +0800

    Sign the user identifiers into headers

--- src/middleware/verifyJWT.ts
+++ src/middleware/verifyJWT.ts
@@ -1,13 +1,13 @@
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 import fs from 'fs';
-import { ReqValue, ExpressRes, ExpressNext, DecodedValue } from '..';
+import { Request, Response, NextFunction, DecodedValue } from '..';
 
-const verifyJWT = (req: ReqValue, res: ExpressRes, next: ExpressNext) => {
+const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
     const cookies = req.cookies;
     if (!cookies?.access) return res.sendStatus(401);
     const token = cookies.access;
-
+    
     const pubkey = fs.readFileSync(process.env.public_key!, 'utf8');
     jwt.verify(token, pubkey, { algorithms: ['RS256'] }, (err, decoded: DecodedValue) => {
         if (err && err.message === 'jwt expired') {
@@ -17,9 +17,9 @@ const verifyJWT = (req: ReqValue, res: ExpressRes, next: ExpressNext) => {
         if (err || !(decoded?.userId) || !(decoded?.username)) {
             return res.sendStatus(403);// invalid token
         }
-
-        req.userId = decoded.userId;
-        req.username = decoded.username;
+        
+        req.headers['userId'] = decoded.userId;
+        req.headers['username'] = decoded.username;
         next();
     });
 };
