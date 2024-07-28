e3b28358aac3fca70f0ede801c6f3dbf8b27ec9c
libright1558 <n32717325@gmail.com>
2024-07-28 23:20:22 +0800

    Modify functions for separating frontend and backend

--- src/controller/loginController.ts
+++ src/controller/loginController.ts
@@ -11,43 +11,21 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-const renderLoginPage = (req: Request, res: Response) => {
-    try {
-        return res.status(200).render('login');
-    } catch (e) {
-        console.log('renderLoginPage error: ', e);
-        return res.sendStatus(500);
-    }
-};
-
 const userLogin = async (req: Request, res: Response) => {
-    const payload = req.body;
     try {
         if (!req.body.logUsername || !req.body.logPassword) {
-            payload.errorMessage = 'Make sure each field has a valid value.';
-            return res.status(400).render('login', payload);
+            return res.sendStatus(400);
         }
 
         const user = await findOne(req.body.logUsername) as FindOneType[] | undefined;
 
-        if (user) {
-            if (user[0] === null || user[0] === undefined) {
-                payload.errorMessage = 'username or password incorrect.';
-                return res.status(401).render('login', payload);
-            }
-    
+        if (user && user[0]) {
+            
             const result = await bcrypt.compare(req.body.logPassword, user[0].password);
     
             if (result === true) {
 
                 const privkey = fs.readFileSync(`${process.env.private_key}`, 'utf8');
-    
-                const accessToken = jwt.sign({ userId: user[0].userId, username: user[0].username }
-                    , privkey
-                    , {
-                        algorithm: 'RS256',
-                        expiresIn: '60s'
-                    });
     
                 const refreshToken = jwt.sign({ userId: user[0].userId, username: user[0].username }
                     , privkey
@@ -56,22 +34,18 @@ const userLogin = async (req: Request, res: Response) => {
                         expiresIn: '7d'
                     });
     
-                res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: true, maxAge: 7 * 24 * 60 * 60 * 1000 });
-                res.cookie('access', accessToken, { httpOnly: true, sameSite: true, maxAge: 2 * 60 * 1000 });
-                return res.redirect(200, '/');
+                res.set('Authorization', `Bearer ${refreshToken}`);
+                return res.sendStatus(200);
             }
         }
         
-        payload.errorMessage = 'username or password incorrect.';
-        return res.status(401).render('login', payload);
+        return res.sendStatus(401);
     } catch (e) {
         console.log('userLogin error', e);
-        payload.errorMessage = 'userLogin error.';
-        return res.status(500).render('login', payload);
+        return res.sendStatus(500);
     }
 };
 
 export default {
-    renderLoginPage,
     userLogin
 };
