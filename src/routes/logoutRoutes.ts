30d4547611d04ea4f8a8b72632e2fa208d661db7
libright1558 <n32717325@gmail.com>
2024-07-28 23:13:54 +0800

    Move login and logout functions to frontend

--- src/routes/logoutRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import logoutController from '../controller/logoutController.js';
-const router = express.Router();
-
-router.route('/')
-    .get(logoutController.logout);
-
-export default router;
