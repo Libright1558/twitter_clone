296fbdcc7296cd4814a42edfe5fb3cb26eaf26f8
libright1558 <n32717325@gmail.com>
2024-07-28 23:17:51 +0800

    Modify cookie to authorization header

--- src/middleware/verifyJWT.ts
+++ src/middleware/verifyJWT.ts
@@ -4,18 +4,19 @@ import fs from 'fs';
 import { Request, Response, NextFunction, DecodedValue } from '..';
 
 const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
-    const cookies = req.cookies;
-    if (!cookies?.access) return res.sendStatus(401);
-    const token = cookies.access;
+    const bearerHeader = req.headers['Authorization'] as string;
+    const token = bearerHeader?.split(' ');
+    if (!token) return res.sendStatus(401);
+    const accessToken = token[1];
     
     const pubkey = fs.readFileSync(process.env.public_key!, 'utf8');
-    jwt.verify(token, pubkey, { algorithms: ['RS256'] }, (err, decoded: DecodedValue) => {
+    jwt.verify(accessToken, pubkey, { algorithms: ['RS256'] }, (err, decoded: DecodedValue) => {
         if (err && err.message === 'jwt expired') {
-            return res.status(403).send(err);// token expired
+            return res.status(403).send(err); // token expired
         }
 
         if (err || !(decoded?.userId) || !(decoded?.username)) {
-            return res.sendStatus(403);// invalid token
+            return res.sendStatus(403); // invalid token
         }
         
         req.headers['userId'] = decoded.userId;
