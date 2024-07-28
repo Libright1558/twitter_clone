3ac7bc38501c72d0059eefffc076881ce9f863a1
libright1558 <n32717325@gmail.com>
2024-07-28 23:17:18 +0800

    Remove get method

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -3,7 +3,6 @@ import loginController from '../controller/loginController.js';
 const router = express.Router();
 
 router.route('/')
-    .get(loginController.renderLoginPage)
     .post(loginController.userLogin);
 
 export default router;
